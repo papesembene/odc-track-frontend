d1b5675a542ae9db8712413c32398e0f2a7b9f6f
pape sembene <sembenpape4@gmail.com>
2026-03-01 18:27:44 +0000

    Routing: liste et detail apprenant pole emploi

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginPage from '@/modules/auth/pages/LoginPage.vue'
 import PoleEmploiDashboardPage from '@/modules/pole-emploi/pages/PoleEmploiDashboardPage.vue'
 import ValidationsPage from '@/modules/pole-emploi/pages/ValidationsPage.vue'
+import ApprenantsListPage from '@/modules/pole-emploi/pages/ApprenantsListPage.vue'
+import ApprenantDetailPage from '@/modules/pole-emploi/pages/ApprenantDetailPage.vue'
 import PlaceholderBackofficePage from '@/modules/pole-emploi/pages/PlaceholderBackofficePage.vue'
 
 const router = createRouter({
@@ -10,12 +12,8 @@ const router = createRouter({
     { path: '/login', name: 'login', component: LoginPage },
     { path: '/dashboard', name: 'dashboard', component: PoleEmploiDashboardPage },
     { path: '/validations', name: 'validations', component: ValidationsPage },
-    {
-      path: '/apprenants',
-      name: 'apprenants',
-      component: PlaceholderBackofficePage,
-      props: { title: 'Apprenants', activeMenu: 'apprenants' },
-    },
+    { path: '/apprenants', name: 'apprenants', component: ApprenantsListPage },
+    { path: '/apprenants/:id', name: 'apprenant-detail', component: ApprenantDetailPage },
     {
       path: '/statistiques',
       name: 'statistiques',
